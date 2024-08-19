6b1370bbb6a737d85866ce5417702487c46ce09c
zeCode <yassineamine788@gmail.com>
2024-08-19 17:20:50 +0200

    Adedd Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 
 import { Toaster } from "@/components/ui/toaster";
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <Toaster />
+    <Analytics />
   </React.StrictMode>
 );
