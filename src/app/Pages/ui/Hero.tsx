d5071055a7be373ca89c4017ca4c325fdac38e7d
zeCode <yassineamine788@gmail.com>
2024-10-23 18:40:58 +0200

    some changes

--- src/app/Pages/ui/Hero.tsx
+++ src/app/Pages/ui/Hero.tsx
@@ -2,10 +2,14 @@ export const Hero = () => {
   return (
     <div className="mt-5">
       <div className="text-[16px] leading-[20px] tracking-[-0px] text-[#A1A1A1] mb-4">
-        Hello
+        Ciao
       </div>
-      <div className="text-[42px] leading-[26px] tracking-[-2.4px]">
+      <div className="text-[44px] leading-[26px] tracking-[-2.4px]">
         Hi, I’m Yassine, but Yass is cool too?
+      </div>
+      <div className="font-light text-[22px]  tracking-[-1px] dark:text-[#A1A1A1] max-w-xl mt-8">
+        Always curious, I love figuring out how things work and making them even
+        better.
       </div>
     </div>
   );
--- src/app/Pages/ui/Home.tsx
+++ src/app/Pages/ui/Home.tsx
@@ -3,7 +3,7 @@ import { Hero } from "./Hero";
 
 export const NewHome = () => {
   return (
-    <div className={`relative max-w-[840px] m-auto `}>
+    <div className={`relative max-w-[800px] m-auto`}>
       <div className="p-5">
         <Hero />
       </div>
