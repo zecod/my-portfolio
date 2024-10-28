dcbb1d9bb9a631a36ba70cd2e48c17d2c9fd3891
zeCode <yassineamine788@gmail.com>
2024-10-28 20:24:47 +0100

    adedd links

--- src/app/Pages/ui/sections/Hero.tsx
+++ src/app/Pages/ui/sections/Hero.tsx
@@ -4,7 +4,11 @@ export const Hero = () => {
       <div className="text-[16px] leading-[20px] tracking-[-0px] text-[#A1A1A1] mb-4">
         Ciao
       </div>
-      <div className="text-[48px] leading-[26px] tracking-[-2.4px]">
+      <div className="hidden md:flex text-[48px] leading-[26px] tracking-[-2.4px]">
+        Hi, I’m Yassine, but Yass is cool too?
+      </div>
+
+      <div className="flex md:hidden text-[48px] leading-[46px] tracking-[-2.4px]">
         Hi, I’m Yassine, but Yass is cool too?
       </div>
       <div className="font-light text-[22px]  tracking-[-1px] dark:text-[#A1A1A1] max-w-xl mt-4">
